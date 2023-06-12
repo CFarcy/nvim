9f9843a3bc38758b7d653293d3892081a3615bde
Cédric Farcy <cedric.farcy@makina-corpus.com>
2023-06-13 01:15:22 +0200

    Use rose-pine as default theme

--- after/plugin/themes.lua
+++ after/plugin/themes.lua
@@ -23,10 +23,11 @@ require("catppuccin").setup({
 
 require("rose-pine").setup({
     disable_background = true,
+    disable_float_background = true,
 })
 
 function ColorMe(color)
-    color = color or "gruvbox"
+    color = color or "rose-pine"
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
